0f65a30f1d77f82cbf799761ae12ed5a2ceebf2f
Gabriel Gardini <gabriel.gardini@most.com.br>
2023-02-22 21:50:13 -0300

    add m-2

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
         </Link>
         <Link
           to="/create-post"
-          className="font-inter font-medium bg-[#6469ff] text-white px-4 py-4 rounded-md"
+          className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 m-2 rounded-md"
         >
           Create
         </Link>
